bfcb931cccd052d1402035c48e17e37776565871
adityapatil687 <adityapatil687@gmail.com>
2025-03-03 20:22:50 +0530

    cors patch

--- server/index.js
+++ server/index.js
@@ -8,18 +8,18 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
 // Load environment variables
-app.use(cors({
-  origin: "https://kaanbaan-1.web.app", // Replace with your Firebase URL
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+
 
 // Initialize Express app
 const app = express();
 const PORT = process.env.PORT;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://kaanbaan-1.web.app", // Replace with your Firebase URL
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // MongoDB Connection
